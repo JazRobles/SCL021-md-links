7ed25d4b1e8bd40c6c395114cb0cee1a0666c5f9
Jazmin Robles <jazmasoterapeuta@gamail.com>
2022-10-13 17:15:09 -0300

    la ruta y existe y readFile toma los archivos y devolucion de llamada

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-module.exports = () => {
-  // ...
-};
+#!/usr/bin/env node
+
+//declarando constantes e importando los módulos de nodo
+const fs = require('fs')
+const path = require('path');
+const colors = require("colors");
+const arguments = process.argv 
+console.log({arguments})
+
+
+
+// verificamos si la ruta existe
+  if (fs.existsSync(process.argv [2])) {
+	console.log("existe")
+  }
+   
+// fs.readFile toma la ruta del archivo y la devolución de llamada
+fs.readFile(process.argv [2], (err, data) => {
+
+	// si hay un error, regístra y regresa
+  if (err) {
+		console.error(err)
+		return 
+	};
+
+	
+
+});
+
+
+
+  
+
+
+// // module.exports = () => {
+// //   // ...
+// };
