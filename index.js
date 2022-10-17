dd79e027938859032a06b53a9805eec512b85c0b
Jazmin Robles <jazmasoterapeuta@gamail.com>
2022-10-17 20:07:27 -0300

    se detectan los links y se leen

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const path = require('path');
 const colors = require("colors");
 const arguments = process.argv 
 console.log({arguments})
+const fetch = require("node-fetch");
 
 
 
 // verificamos si la ruta existe
   if (fs.existsSync(process.argv [2])) {
-	console.log("existe")
+	console.log("la ruta existe")
   }
-   
+   //la ruta es absoluta
+  path.isAbsolute(process.argv [2]);
+  console.log("la ruta es absoluta")
+
+//   
+
 // fs.readFile toma la ruta del archivo y la devolución de llamada
 fs.readFile(process.argv [2], (err, data) => {
 
@@ -29,8 +35,36 @@ fs.readFile(process.argv [2], (err, data) => {
 
 
 
+fetch("https://reqres.in/api/users?page=2")
+.then((respuestaExitosa) => {
+  console.log(respuestaExitosa.status)
+})
+.catch((error) => {
+console.log(error)
+})
   
-
+//detectando los links
+  function detectaURLs(message) {
+    var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+    return message.match(urlRegex);
+  }
+  
+  // leer los archivos
+const readFile = (route) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("README.md", "utf8", (err, data) => {
+      console.log("lenyendo archivo", typeof data);
+      if (err) {
+        reject(err);
+      } else if (data) {
+        const detectaUrl = detectaURLs(data);
+        console.log("urls detected", detectaUrl);
+        resolve(detectaUrl);
+      }
+    });
+  });
+};
+readFile().then((data) => console.log(data.length));
 
 // // module.exports = () => {
 // //   // ...
